59793cd4f643f4ea7d721832cf1f16091ffe2aa7
da.budyakov <dmitry130590@gmail.com>
2017-08-22 17:01:07 +0300

    dev

--- art_parser.py
+++ art_parser.py
@@ -17,71 +17,36 @@ class Tree():
     def get_html_tree(self, text_html):
         tree = html.fromstring(text_html)
         self.root = tree.xpath('//body')[0]
-        children_all = self.root.getchildren()
-        children_selected = [cld for cld in children_all if cld.tag != 'script']
-        self.dom[self.root] = {
-            'parent': None,
-            'children': children_selected,
-            'text_len': 0
-        }
-        # Чтобы не делать рекурсию. Очередь для записи нод
-        self._nodes_queue = [{'parent': self.root, 'node': node} for node in children_selected]
-        while self._nodes_queue:
-            self.add_nodes_to_tree(self._nodes_queue[0])
-        # TODO to_delete
-        print('Общее количество узлов в dom дереве:', str(len(self.dom)), '\n')
 
-    def add_nodes_to_tree(self, node):
-        # TODO дублирование
-        children_all = node['node'].getchildren()
-        children_selected = [cld for cld in children_all if cld.tag != 'script']
-        self.dom[node['node']] = {
-            'node': node['node'],
-            'parent': node['parent'],
-            'children': children_selected,
-            'text_len': 0}
-        for child in children_selected:
-            self._nodes_queue.append({'parent': node['node'], 'node': child})
-        self._nodes_queue.remove(node)
+    def find_text_content(self):
+        iterator = self.root.getiterator('p')
+        text_content = [{'text': i.text, 'node': i} for i in iterator if i.text]
+        for i in text_content:
+            parent = i['node'].getparent()
+            i['parent_attr'] = parent.attrib
+        print('g')
 
-    def calculate_text_length(self):
-        for node in self.dom:
-            if node.text is not None:
-                self.total_text_length += len(node.text.strip())
-                self.dom[node]['text_len'] = len(node.text.strip())
-                parent = self.dom[node]['parent']
-                while parent is not None:
-                    self.dom[parent]['text_len'] += len(node.text.strip())
-                    parent = self.dom[parent]['parent']
-        # TODO to_delete
-        print('Общая длина текста', str(self.total_text_length), '\n')
-
-    def calculate_percent_distribution(self):
-        for item in self.dom:
-            self.dom[item]['ratio'] = self.dom[item]['text_len'] / self.total_text_length
-
-    def find_content_blocks(self):
-        children = [self.dom[cld] for cld in self.dom[self.root]['children']]
-        children.sort(key=lambda d: d['ratio'], reverse=True)
-        while children[0]['ratio'] >= 0.5:
-            children = [self.dom[cld] for cld in self.dom[children[0]['node']]['children']]
-            children.sort(key=lambda d: d['ratio'], reverse=True)
-        else:
-            print(children[0])
-            self.find_all_children_of_element(children[0]['node'])
-
-    def find_all_children_of_element(self, node):
-        iterator = node.getiterator()
-        for i in iterator:
-            if i.text is not None:
-                print(i.text)
+    def calc_text_len(self, text_content):
+        total_text_len = 0
+        for block in text_content:
+            total_text_len += len(block['text'])
+        tags = dict()
+        for i in text_content:
+            if i['tag'] not in tags:
+                tags[i['tag']] = len(i['text'])
+            else:
+                tags[i['tag']] += len(i['text'])
+        rating = sorted(tags, key=lambda k: tags[k], reverse=True)
+        for i in text_content:
+            if i['tag'] == rating[0]:
+                print(i['text'])
+        print('ssf')
 
 class Page():
 
     def __init__(self):
         self.headers={}
         self.text_html = None
-        # self.html_tree = None
         self.tree = Tree()
 
     def get(self, url):
@@ -90,13 +55,9 @@ class Page():
         self.tree.get_html_tree(self.text_html)
 
     def extract_content(self):
-        self.tree.calculate_text_length()
-        self.tree.calculate_percent_distribution()
-        self.tree.find_content_blocks()
-        # self.tree.find_all_children_of_element()
+        self.tree.find_text_content()
 
 if __name__ == '__main__':
     page = Page()
-    page.get('https://lenta.ru/articles/2017/08/21/cenistroyka/')
+    page.get('https://rg.ru/2017/08/22/reg-cfo/kalashnikov-pokazal-neletalnoe-oruzhie-novgo-pokoleniia.html')
     page.extract_content()
-    print('The end')
