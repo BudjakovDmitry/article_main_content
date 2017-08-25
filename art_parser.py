43466dfdd084ec5eb8b14d5a369aa469def5be32
da.budyakov <dmitry130590@gmail.com>
2017-08-25 09:42:12 +0300

    dev

--- art_parser.py
+++ art_parser.py
@@ -9,18 +9,23 @@ https://lenta.ru
 
 import requests
 import os
+import argparse
 from lxml import html
-from uuid import uuid4
+from configparser import ConfigParser
 
 text_tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+config = ConfigParser()
+cwd = os.getcwd()
+config.read(os.path.join(cwd, 'config.ini'))
 
-# TODO добавить описания модуля, классов и методов
+
 class Tree:
     """
     Класс, моделирующий DOM-дерево страницы
     """
     def __init__(self):
         self.body = None
+        self.head = None
         self.total_text_length = 0
         self.dom = dict()
         self.content_blocks = None
@@ -32,6 +37,7 @@ class Tree:
         """
         tree = html.fromstring(html_text)
         self.body = tree.xpath('//body')[0]
+        self.head = tree.xpath('//head')[0]
 
     def find_content_nodes(self, node_name='p'):
         """
@@ -49,6 +55,8 @@ class Tree:
         :return: список, содержащий экземпляры контейнеров, в которых находится текст статьи
         """
         parent_blocks = list()
+        main_blocks = list()
+        class_keywords = ['article', 'body']
         for b in self.content_blocks:
             parent = b['node'].getparent()
             parent_info = {'tag': parent.tag, 'attrib': parent.attrib}
@@ -59,8 +67,13 @@ class Tree:
             block['xpath'] = xpath
             node = self.body.xpath(xpath)[0]
             block['text_len'] = len(node.text_content())
-        parent_blocks.sort(key=lambda k: k['text_len'], reverse=True)
-        main_blocks = self.body.xpath(parent_blocks[0]['xpath'])
+            # ищем по ключевым словам класса
+            for kw in class_keywords:
+                if kw in block['attrib']['class'] and block not in main_blocks:
+                    main_blocks = (self.body.xpath(parent_blocks[0]['xpath']))
+        if len(main_blocks) == 0:
+            parent_blocks.sort(key=lambda k: k['text_len'], reverse=True)
+            main_blocks = self.body.xpath(parent_blocks[0]['xpath'])
         return main_blocks
 
     def find_main_text(self, main_blocks):
@@ -69,13 +82,45 @@ class Tree:
         :param main_blocks: список блоков, в которых находится свтатья
         :return: статья, разделённая по блокам
         """
-        text_blocks = list()
+        content = {
+            'text_blocks': list(),
+            'title': None,
+            'tags': list()
+        }
+        title = self.head.xpath('//title')[0]
+        content['title'] = title.text_content()
         for block in main_blocks:
             iterator = block.getiterator()
             for item in iterator:
-                if item.text_content() is not None and item.tag in text_tags:
-                    text_blocks.append({'tag': item.tag, 'text': item.text_content()})
-        return text_blocks
+                # try нужна для того чтобы исключить блоки с html-комментариями. Из-за них парсер падает.
+                try:
+                    text_content = item.text_content()
+                    tag = item.tag
+                except ValueError:
+                    continue
+                if text_content is not None and tag in text_tags:
+                    links = list()
+                    for child in item.getchildren():
+                        if child.tag == 'a':
+                            link = {
+                                'text': child.text_content(),
+                                'href': child.attrib['href']}
+                            links.append(link)
+                    content['text_blocks'].append({'tag': item.tag, 'text': item.text_content(), 'links': links})
+                    if item.tag not in content['tags']:
+                        content['tags'].append(item.tag)
+        headers = self.find_headers()
+        if 'h1' not in content['tags'] and headers is not None:
+            content['text_blocks'].insert(0, headers)
+        return content
+
+    def find_headers(self):
+        headers = {'tag': None, 'text': None, 'links': list()}
+        header_obj = self.body.xpath('//h1')
+        if len(header_obj) > 0:
+            headers['tag'] = header_obj[0].tag
+            headers['text'] = header_obj[0].text_content().upper()
+            return headers
 
 
 class Page:
@@ -120,7 +165,7 @@ class Text:
         Форматрует текст по ширине строки
         :param line_width: максимальная ширина строки в символах.
         """
-        for block in self.text:
+        for block in self.text['text_blocks']:
             words = block['text'].split()
             width = 0
             for i, word in enumerate(words):
@@ -135,27 +180,68 @@ class Text:
         Добавляет поля между абзацами и заголовками
         :param number_of_lines: вуличина поля, выраженная в количестве пустых строк
         """
-        for block in self.text:
+        for block in self.text['text_blocks']:
+            block['text'].strip()
+            if block['text'] == '':
+                continue
             block['text'] += '\n'*(number_of_lines + 1)
 
+    def decorate_links(self):
+        for block in self.text['text_blocks']:
+            for link in block['links']:
+                start_text_index = block['text'].find(link['text'])
+                end_text_index = start_text_index + len(link['text'])
+                pre = block['text'][:end_text_index ]
+                aft = block['text'][end_text_index:]
+                # block['text'] = pre + '['link['href'] + aft
+                block['text'] = '{0} [{1}] {2}'.format(pre, link['href'], aft)
+
+    def chec_file_name(self, file_name):
+        forbidden_symbols = ['~', '#' '%', '*', '{', '}', '\\', ':', '<', '>', '?', '/', '"']
+        for symbol in forbidden_symbols:
+            if symbol in file_name:
+                file_name = file_name.replace(symbol, ' ')
+        return file_name
+
     def save(self):
-        cwd = os.getcwd()
-        folder = uuid4().hex
-        path = os.path.join(cwd, folder)
-        if not os.path.exists(path):
-            os.makedirs(path)
-            path = os.path.join(path, 'art.txt')
-        f = open(path, 'w')
-        for i in self.text:
-            f.write(i['text'])
-        f.close()
+        dir_path = config['GENERAL']['result_dir']
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        name = self.chec_file_name(self.text['title'])
+        file_name = '{0}.txt'.format(name)
+        full_path = os.path.join(dir_path, file_name)
+        with open(full_path, 'w') as art:
+            for i in self.text['text_blocks']:
+                art.write(i['text'])
 
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', '-u')
+    return parser
+"""
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    url = args.url
+    if url:
+        page = Page()
+        page.get(url)
+        content = page.extract_content()
+        text = Text(content)
+        text.decorate_links()
+        text.set_line_width()
+        text.add_margins()
+        text.save()
+    else:
+        print('Ошибка: Не задан обязательный аргумент --url')
+"""
 if __name__ == '__main__':
     page = Page()
-    page.get('https://lenta.ru/news/2017/08/23/arrest/')
+    page.get('https://ria.ru/world/20170825/1501052126.html')
     content = page.extract_content()
     text = Text(content)
+    text.decorate_links()
     text.set_line_width()
     text.add_margins()
     text.save()
