fbee0da1e5309fca90d2c3d65a28e4ac67ced4d9
da.budyakov <dmitry130590@gmail.com>
2017-08-25 09:41:12 +0300

    art_parser.py

--- art_parser.py
+++ art_parser.py
@@ -14,99 +14,129 @@ from uuid import uuid4
 
 text_tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
-# TODO разобраться, от чего должен наследоваться этот класс (object?)
 # TODO добавить описания модуля, классов и методов
-class Tree():
-
+class Tree:
+    """
+    Класс, моделирующий DOM-дерево страницы
+    """
     def __init__(self):
-        self.root = None
+        self.body = None
         self.total_text_length = 0
         self.dom = dict()
-        self.text_content = None
+        self.content_blocks = None
 
-    def get_html_root(self, text_html):
-        tree = html.fromstring(text_html)
-        self.root = tree.xpath('//body')[0]
+    def get_body(self, html_text):
+        """
+        Создаёт экземпляр объекта body, моделирующий тело страницы.
+        :param html_text: html код страницы в текстовом формате
+        """
+        tree = html.fromstring(html_text)
+        self.body = tree.xpath('//body')[0]
 
     def find_content_nodes(self, node_name='p'):
+        """
+        Находит в теле страницы блоки, которые содержат основной контент
+        :param node_name: название тега, в котором содержится текст статьи
+        """
         # На первой итерации считаем, что весь контент статьи находится в блоках <p>..</p>
-        iterator = self.root.getiterator(node_name)
-        self.text_content = [{'text': it.text, 'node': it} for it in iterator if it.text]
+        iterator = self.body.getiterator(node_name)
+        self.content_blocks = [{'text': it.text_content(), 'node': it} for it in iterator if it.text_content()]
 
     def find_main_blocks(self):
+        """
+        Находит родительские контейнеры для блоков с текстом статьи.
+        Если родительских блоков несколько, то по размеру текста определяет, в каком блоке основной контент
+        :return: список, содержащий экземпляры контейнеров, в которых находится текст статьи
+        """
         parent_blocks = list()
-        for i in self.text_content:
-            parent = i['node'].getparent()
-            parent_discr = {'tag': parent.tag, 'attrib': parent.attrib}
-            if parent_discr not in parent_blocks:
-                parent_blocks.append(parent_discr)
+        for b in self.content_blocks:
+            parent = b['node'].getparent()
+            parent_info = {'tag': parent.tag, 'attrib': parent.attrib}
+            if parent_info not in parent_blocks:
+                parent_blocks.append(parent_info)
         for block in parent_blocks:
             xpath = '//{0}[@class="{1}"]'.format(block['tag'], block['attrib']['class'])
             block['xpath'] = xpath
-            block['text_len'] = len(self.root.xpath(xpath)[0].text_content())
+            node = self.body.xpath(xpath)[0]
+            block['text_len'] = len(node.text_content())
         parent_blocks.sort(key=lambda k: k['text_len'], reverse=True)
-        main_blocks = self.root.xpath(parent_blocks[0]['xpath'])
-        # content = self.root.xpath(main_blocks['xpath'])[0].text_content()
-        return self.find_main_text(main_blocks)
+        main_blocks = self.body.xpath(parent_blocks[0]['xpath'])
+        return main_blocks
 
     def find_main_text(self, main_blocks):
-        text = list()
+        """
+        Находит текстовые блоки внутри главного блока, который включаев всю статью
+        :param main_blocks: список блоков, в которых находится свтатья
+        :return: статья, разделённая по блокам
+        """
+        text_blocks = list()
         for block in main_blocks:
             iterator = block.getiterator()
             for item in iterator:
-                if item.text is not None and item.tag in text_tags:
-                    text.append({'tag': item.tag, 'text': item.text})
-        return text
+                if item.text_content() is not None and item.tag in text_tags:
+                    text_blocks.append({'tag': item.tag, 'text': item.text_content()})
+        return text_blocks
 
-    def calc_text_len(self, text_content):
-        total_text_len = 0
-        for block in text_content:
-            total_text_len += len(block['text'])
-        tags = dict()
-        for i in text_content:
-            if i['tag'] not in tags:
-                tags[i['tag']] = len(i['text'])
-            else:
-                tags[i['tag']] += len(i['text'])
-        rating = sorted(tags, key=lambda k: tags[k], reverse=True)
-        for i in text_content:
-            if i['tag'] == rating[0]:
-                print(i['text'])
 
-class Page():
+class Page:
+    """
+    Класс, моделирующий страницу
+    """
 
     def __init__(self):
         self.headers={}
-        self.text_html = None
+        self.html_text = None
         self.tree = Tree()
 
     def get(self, url):
+        """
+        Получает страницу по указанному url, сохраниет html-текст и экземпляр объекта body.
+        :param url: адрес страницы
+        """
         resp = requests.get(url, headers=self.headers)
-        self.text_html = resp.text
-        self.tree.get_html_root(self.text_html)
+        self.html_text = resp.text
+        self.tree.get_body(self.html_text)
 
     def extract_content(self):
+        """
+        Извлекает из страницы основной контент.
+        :return: основной контент, разделённый на контейнеры.
+        """
         self.tree.find_content_nodes()
-        return self.tree.find_main_blocks()
+        main_blocks = self.tree.find_main_blocks()
+        main_content = self.tree.find_main_text(main_blocks)
+        return main_content
 
 
-class Text():
-
+class Text:
+    """
+    Класс для форматирования текста
+    """
     def __init__(self, text):
         self.text = text
 
-    def format(self):
-        # Блоки отбиваем пустой строкой
+    def set_line_width(self, line_width=80):
+        """
+        Форматрует текст по ширине строки
+        :param line_width: максимальная ширина строки в символах.
+        """
         for block in self.text:
             words = block['text'].split()
-            total_len = 0
+            width = 0
             for i, word in enumerate(words):
-                total_len += len(word) + 1
-                if total_len > 80:
+                width += len(word) + 1
+                if width > line_width:
+                    width = len(word) + 1
                     words[i] = '\n' + words[i]
-                    total_len = len(word) + 1
-            block['text'] = ' '.join(words) + '\n\n'
+            block['text'] = ' '.join(words)
 
+    def add_margins(self, number_of_lines=1):
+        """
+        Добавляет поля между абзацами и заголовками
+        :param number_of_lines: вуличина поля, выраженная в количестве пустых строк
+        """
+        for block in self.text:
+            block['text'] += '\n'*(number_of_lines + 1)
 
     def save(self):
         cwd = os.getcwd()
@@ -115,7 +145,6 @@ class Text():
         if not os.path.exists(path):
             os.makedirs(path)
             path = os.path.join(path, 'art.txt')
-        self.format()
         f = open(path, 'w')
         for i in self.text:
             f.write(i['text'])
@@ -124,7 +153,9 @@ class Text():
 
 if __name__ == '__main__':
     page = Page()
-    page.get('https://www.gazeta.ru/politics/2017/08/22_a_10845614.shtml')
+    page.get('https://lenta.ru/news/2017/08/23/arrest/')
     content = page.extract_content()
     text = Text(content)
+    text.set_line_width()
+    text.add_margins()
     text.save()
